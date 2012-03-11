95caf712948807f658268391ec8ec8c95ee796bf
Jason Moore <moorepants@gmail.com>
2012-03-11 14:37:39 -0700

    Adding the plot code.

---
+++ bode.py
@@ -0,0 +1,61 @@
+from numpy import logspace, sqrt, array, zeros, log10, rad2deg
+from matplotlib import rcParams
+from bicycleid import data, plot, model
+from dtk import control
+
+params = {'axes.labelsize': 8,
+          'text.fontsize': 8,
+          'legend.fontsize': 6,
+          'xtick.labelsize': 8,
+          'ytick.labelsize': 8,
+          'text.usetex': True}
+rcParams.update(params)
+
+subDef = {}
+subDef['Rider'] = ['Charlie', 'Jason', 'Luke']
+subDef['Environment'] = ['Treadmill', 'Pavilion']
+subDef['Maneuver'] = ['Balance', 'Track Straight Line',
+    'Balance With Disturbance', 'Track Straight Line With Disturbance']
+subDef['Speed'] = ['4.0']
+subDef['MeanFit'] = 0.0
+subDef['Duration'] = 0.0
+
+w = logspace(-1, 2, num=100)
+dat = data.ExperimentalData(w=w)
+
+models = {rider: model.Whipple(rider) for rider in ['Charlie']}
+
+bodePlot = plot.BodePlot(w)
+bodeData = dat.subset_bode(**subDef)
+bodePlot.update_graph(bodeData, models)
+
+# arm model at the mean speed for this run
+
+A = array([[0.0, 0.0, 1.0, 0.0],
+           [0.0, 0.0, 0.0, 1.0],
+           [8.4999, -12.2999, -0.0393, -1.1386],
+           [7.1271,  0.3462, 2.5343, -7.3279]])
+
+B = array([[0.0],
+           [0.0],
+           [-0.0314],
+           [2.0233]])
+C = array([[1.0, 0.0, 0.0, 0.0]])
+D = zeros((1, 1))
+sys = control.StateSpace(A, B, C, D)
+mag, phase = bodePlot.bode.mag_phase_system(sys)
+
+bodePlot.bode.figs[0].axes[0].lines[4].set_ydata(20 * log10(mag[:, 0, 0]))
+bodePlot.bode.figs[0].axes[1].lines[4].set_ydata(rad2deg(phase[:, 0, 0]))
+
+bodePlot.bode.figs[0].axes[0].set_ylim((-120, 0))
+bodePlot.bode.figs[0].axes[1].set_ylim((-250, 0))
+
+goldenRatio = (1 + sqrt(5.)) / 2.
+columnWidthPt = 257.28102
+figWidth = columnWidthPt / 72.27
+figHeight = figWidth / goldenRatio
+
+bodePlot.bode.figs[0].set_figwidth(figWidth)
+bodePlot.bode.figs[0].set_figheight(figHeight)
+bodePlot.bode.figs[0].savefig('figures/bode.pdf')
---
+++ coefficient_plot.py
@@ -0,0 +1,55 @@
+from numpy import linspace, sqrt, zeros
+from scipy.io import loadmat
+from matplotlib import rcParams
+from bicycleid import data, plot, model
+
+params = {'axes.labelsize': 10,
+          'text.fontsize': 10,
+          'legend.fontsize': 10,
+          'xtick.labelsize': 8,
+          'ytick.labelsize': 8,
+          'text.usetex': True}
+rcParams.update(params)
+
+dat = data.ExperimentalData()
+
+coefPlot = plot.CoefficientPlot()
+
+subDef = {}
+subDef['Rider'] = ['Charlie', 'Jason', 'Luke']
+subDef['Environment'] = ['Treadmill', 'Pavilion']
+subDef['Maneuver'] = ['Balance', 'Track Straight Line',
+    'Balance With Disturbance', 'Track Straight Line With Disturbance']
+subDef['Speed'] = ['1.4', '2.0', '3.0', '4.0', '4.92', '5.8', '7.0', '9.0']
+subDef['MeanFit'] = 0.0
+subDef['Duration'] = 0.0
+
+subDat = dat.subset(**subDef)
+
+speedRange = linspace(0.0, 10.0, num=50)
+models = {rider: model.Whipple(rider).matrices(speedRange) for rider in ['Charlie']}
+
+coefPlot.update_graph(subDat, models)
+
+# now add the whipple model
+m = loadmat('armsAB.mat', squeeze_me=True) # this is charlie at 11 speeds
+
+stateMats = zeros((11, 4, 4))
+for i, A in enumerate(m['stateMatrices']):
+    stateMats[i] = A[[3, 6, 16, 18], :][:, [3, 6, 16, 18]]
+
+inputMats = zeros((11, 4, 2))
+for i, B in enumerate(m['inputMatrices']):
+    inputMats[i] = B[[3, 6, 16, 18], 2:4]
+
+for lab, ax in coefPlot.axes.items():
+    row, col = int(lab[-2]), int(lab[-1])
+    if lab[0] == 'a':
+        ax.plot(m['speed'], stateMats[:, row - 1, col - 1])
+    elif lab[0] == 'b':
+        ax.plot(m['speed'], inputMats[:, row - 1, col - 1])
+
+coefPlot.figure.set_figwidth(7.5)
+goldenRatio = (sqrt(5)-1.0)/2.0
+coefPlot.figure.set_figheight(7.5 * goldenRatio)
+coefPlot.figure.savefig('figures/coefficients.pdf')
